0eea87d01d3ab5bb47813902cfa4bc13ea0c3744
led12138 <435753428@qq.com>
2017-07-17 17:46:09 +0800

    ledzhaoshangjia

--- src/Components/Business_One/index.js
+++ src/Components/Business_One/index.js
@@ -1,3 +1,23 @@
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 import React,{Component} from "react";
 import "./index.scss";
 import "../../assets/iconfont/iconfont.css";
@@ -7,7 +27,7 @@ import {
 
 
 
-class Business_One extends Component{
+class Business_Case extends Component{
 	constructor(props) {
 		super(props);
 		
@@ -22,11 +42,139 @@ class Business_One extends Component{
 					{
 						this.props.children
 					}
-				</section>
+				</section>	
+
+				<div id="daohang">
+					<ul>
+						<li>婚礼策划<i className="iconfont more_unfold">&#xe6a6;</i></li>
+						<li>默认排序<i className="iconfont more_unfold">&#xe6a6;</i></li>
+					</ul>
+				</div>
+
+				<div className="line"></div>
+
+				<div id="cont">
+					<ul>
+						<li>
+							<div><img src="src/img/tp3.jpg"/></div>
+							<div className="text">
+								<h3>IRIS-weddig丨法式福楼丨日式樱花主题</h3>
+								<p className="p1">IRIS独立婚礼策划团队</p>
+								<p className="p2">
+									<img className="star" src="src/img/star.png"/>
+									47
+									<span></span>
+								</p>
+							</div>
+						</li>
+					</ul>
+				</div>
+
+				<div id="cont">
+					<ul>
+						<li>
+							<div><img src="src/img/tp3.jpg"/></div>
+							<div className="text">
+								<h3>IRIS-weddig丨法式福楼丨日式樱花主题</h3>
+								<p className="p1">IRIS独立婚礼策划团队</p>
+								<p className="p2">
+									<img className="star" src="src/img/star.png"/>
+									47
+									<span></span>
+								</p>
+							</div>
+						</li>
+					</ul>
+				</div>
+
+				<div id="cont">
+					<ul>
+						<li>
+							<div><img src="src/img/tp3.jpg"/></div>
+							<div className="text">
+								<h3>IRIS-weddig丨法式福楼丨日式樱花主题</h3>
+								<p className="p1">IRIS独立婚礼策划团队</p>
+								<p className="p2">
+									<img className="star" src="src/img/star.png"/>
+									47
+									<span></span>
+								</p>
+							</div>
+						</li>
+					</ul>
+				</div>
+
+				<div id="cont">
+					<ul>
+						<li>
+							<div><img src="src/img/tp3.jpg"/></div>
+							<div className="text">
+								<h3>IRIS-weddig丨法式福楼丨日式樱花主题</h3>
+								<p className="p1">IRIS独立婚礼策划团队</p>
+								<p className="p2">
+									<img className="star" src="src/img/star.png"/>
+									47
+									<span></span>
+								</p>
+							</div>
+						</li>
+					</ul>
+				</div>
+
+				<div id="cont">
+					<ul>
+						<li>
+							<div><img src="src/img/tp3.jpg"/></div>
+							<div className="text">
+								<h3>IRIS-weddig丨法式福楼丨日式樱花主题</h3>
+								<p className="p1">IRIS独立婚礼策划团队</p>
+								<p className="p2">
+									<img className="star" src="src/img/star.png"/>
+									47
+									<span></span>
+								</p>
+							</div>
+						</li>
+					</ul>
+				</div>
+
+				<div id="cont">
+					<ul>
+						<li>
+							<div><img src="src/img/tp3.jpg"/></div>
+							<div className="text">
+								<h3>IRIS-weddig丨法式福楼丨日式樱花主题</h3>
+								<p className="p1">IRIS独立婚礼策划团队</p>
+								<p className="p2">
+									<img className="star" src="src/img/star.png"/>
+									47
+									<span></span>
+								</p>
+							</div>
+						</li>
+					</ul>
+				</div>
+
+				<div id="cont">
+					<ul>
+						<li>
+							<div><img src="src/img/tp3.jpg"/></div>
+							<div className="text">
+								<h3>IRIS-weddig丨法式福楼丨日式樱花主题</h3>
+								<p className="p1">IRIS独立婚礼策划团队</p>
+								<p className="p2">
+									<img className="star" src="src/img/star.png"/>
+									47
+									<span></span>
+								</p>
+							</div>
+						</li>
+					</ul>
+				</div>
 					
 			</div>
 		)
 	}
 }
 
-export default Business_One;
+export default Business_Case;
